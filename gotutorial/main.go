da1286b2f2bfbee5500711ba800f7ea3a2b2b30d
Eric Weise <eweise1998@gmail.com>
2024-01-22 13:21:46 -0500

    Initial copying of instructor's repo

---
+++ gotutorial/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	RunBasicIODemo()
+	RunConditionalDemo()
+	RunTypesAndInterfacesDemo()
+	RunConstantsDemo()
+	RunTypeAliasDemo()
+	RunArraySliceMapDemo()
+	RunLoopDemo()
+	RunFunctionsDemo()
+	RunBasicPointersDemo()
+	RunStructsDemo()
+	RunObjectReceiverDemo()
+	RunPackagesDemo()
+	RunErrorsDemo()
+	RunOopDemo()
+	RunInterfaceDemo()
+	RunFileIODemo()
+	RunJsonDemo()
+}
